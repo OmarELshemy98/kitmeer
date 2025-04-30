f393a62f75c53c74dbf992f9febf0818e35547fd
Omar Elshemy <omar.ibrahim@arconme.com>
2025-05-01 01:35:56 +0300

    finish the content of home page

--- js/home.js
+++ js/home.js
@@ -57,24 +57,21 @@ const swiper = new Swiper('.swiper', {
     },
     breakpoints: {
         1280: {
-            slidesPerView: 4,  // تم تغييرها من 3 إلى 4
-            spaceBetween: 10   // تقليل المسافة بين العناصر
+            slidesPerView: 4,  
+            spaceBetween: 10   
         }
     }
 });
 
 const newsCarousel = new Swiper('.news-carousel', {
-    slidesPerView: 2,
+    slidesPerView: 1,
     spaceBetween: 30,
+    loop: true,
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
     breakpoints: {
-        320: {
-            slidesPerView: 1,
-            spaceBetween: 20
-        },
         768: {
             slidesPerView: 2,
             spaceBetween: 30
